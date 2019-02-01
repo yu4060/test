3c94ad64065d9d8d218023faec2586f78e6a064b
yu4060 <yu4060@gmail.com>
2019-02-01 23:13:19 +0900

    57やりなおした

--- 57.py
+++ 57.py
@@ -1,1 +1,23 @@
-間違えてコードを消失してしまいました
+#57. 係り受け解析
+#Stanford Core NLPの係り受け解析の結果（collapsed-dependencies）を有向グラフとして可視化せよ．可視化には，係り受け木をDOT言語に変換し，Graphvizを用いるとよい．また，Pythonから有向グラフを直接的に可視化するには，pydotを使うとよい．
+
+
+
+
+from xml.etree import ElementTree
+import pydot
+
+tree = ElementTree.parse('/Users/yukimatsuo/Desktop/untitled/nlp.txt.xml')
+root = tree.getroot()
+
+edges = []
+for i, sentence in enumerate(root.iter('sentence')):
+    for dep in sentence.iterfind('./dependencies[@type="collapsed-dependencies"]/dep'):
+        governor = dep.findtext('governor')
+        dependent = dep.findtext('dependent')
+        if dependent != '.' and dependent != ',':
+            edges.append((governor, dependent))
+    if i == 3:
+        g = pydot.graph_from_edges(edges, directed = True)
+        g.write_jpeg("{}.jpg".format(i), prog='dot')
+        edges =[]
